f8d35b52d8dcd991c09c67dafcb1dca46fd16f21
McGavyn.Barnett <157079533+McGavyn@users.noreply.github.com>
2024-03-28 13:34:10 -0600

    dimples

--- Week9/sketch.js
+++ Week9/sketch.js
@@ -4,7 +4,7 @@ function setup() {
 
 function draw() {
       background(120,45,78);
-    textSize(18)
+    textSize(18);
     text("I love Video Games", 350,80);
 
     // head
@@ -12,12 +12,15 @@ function draw() {
     circle(250,100,175);
    
     // eyes
-    strokeWeight(10);
-    fill(0);
-    point(220,75);
-    point(285,75);
+    strokeWeight(2);
+    fill(173, 216, 230);
+    circle(220,75,10);
+    circle(285,75,10);
   
+
   //glasses
+   strokeWeight(10);
+  fill(0);
  line(205,85,205,65);
   
   line(205,90,235,90);
@@ -44,17 +47,37 @@ function draw() {
     
     // mouth
     ellipse(255, 135, 20,1);
+    line(245,135,240,130);
+    line(265,135,270,130);
+  
+  //dimples
+    point(225,125);
+    point(285,125);
+  
 
     // hair
     fill(150,75,0);
-  ellipse(250,20,140,50)
+  ellipse(250,20,140,50);
     // body
-    fill(10, 24, 120);
+    fill(128,128,128);
     rect(200,185,100,150);
     
     // decoration
+    strokeWeight(2);
+    fill(225);
+    circle(250,205,10);
+    circle(250,220,10);
+    circle(250,235,10);
+    circle(250,250,10);
+    circle(250,265,10);
+    circle(250,280,10);
+    circle(250,295,10);
+    circle(250,310,10);
+    fill(128,128,128);
+    ellipse(225,220,25,12);
    
     // right arm
+    strokeWeight(10);
     rect(300,195,50,10);
     line(350,200,300,265);
     circle(300,265,10);
@@ -70,7 +93,7 @@ function draw() {
   
     // right leg
     rect(290,335,10,70);
-    rect(270,395,15,10)
+    rect(270,395,15,10);
     
     fill(175);
     textSize(22);
